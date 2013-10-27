f75798e702584e71ca6c036adc9a4aada59be1f4
Yuki Matsukura <matsubokkuri@gmail.com>
2013-10-28 06:39:25 +0900

    Added setter for operation.

--- src/DMM/Affiliate.php
+++ src/DMM/Affiliate.php
@@ -13,6 +13,9 @@ class Affiliate
     private $word;
     private $service;
 
+    private $operation = 'ItemList';
+    private $site = 'DMM.co.jp';
+
     /**
      * __construct
      *
@@ -52,6 +55,30 @@ class Affiliate
     }
 
     /**
+     * setOperation
+     *
+     * @param string $operation
+     * @access public
+     * @return void
+     */
+    public function setOperation($operation = 'ItemList')
+    {
+        $this->operation = $operation;
+    }
+
+    /**
+     * setSite
+     *
+     * @param string $site
+     * @access public
+     * @return void
+     */
+    public function setSite($site = 'DMM.co.jp')
+    {
+        $this->site = $site;
+    }
+
+    /**
      * getResult
      *
      * @access public
@@ -59,9 +86,11 @@ class Affiliate
      */
     public function getResult()
     {
-        $contents = file_get_contents(sprintf('http://affiliate-api.dmm.com/?api_id=%s&affiliate_id=%s&operation=ItemList&version=2.00&timestamp=2012-01-13%%2014%%3A08%%3A16&site=DMM.co.jp&service=%s&keyword=%s',
+        $contents = file_get_contents(sprintf('http://affiliate-api.dmm.com/?api_id=%s&affiliate_id=%s&operation=%s&version=2.00&timestamp=2012-01-13%%2014%%3A08%%3A16&site=%s&service=%s&keyword=%s',
             $this->api_id,
             $this->affiliate_id,
+            $this->operation,
+            $this->site,
             $this->service,
             urlencode(mb_convert_encoding($this->word, 'EUC-JP', 'UTF-8'))));
 
