db4ee267e91d4b95641377a174c0b319a600cf10
Yuki Matsukura <matsubokkuri@gmail.com>
2013-11-03 03:32:56 +0900

    Added full support api

--- src/DMM/Affiliate.php
+++ src/DMM/Affiliate.php
@@ -6,14 +6,24 @@ namespace DMM;
  */
 class Affiliate
 {
+    const HOST = 'affiliate-api.dmm.com';
     const SERVICE_DIGITAL = 'digital';
+    const VERSION = '2.00';
 
     private $api_id;
     private $affiliate_id;
     private $word;
     private $service;
+    private $floor;
+    private $offset;
+    private $sort;
 
     private $operation = 'ItemList';
+
+    /**
+     * - DMM.co.jp: DMM R18
+     * - DMM.com: DMM All
+     */
     private $site = 'DMM.co.jp';
 
     /**
@@ -55,6 +65,40 @@ class Affiliate
     }
 
     /**
+     * setFloor
+     *
+     * @param mixed $floor
+     * @access public
+     * @return void
+     */
+    public function setFloor($floor = null)
+    {
+        $This->floor = $floor;
+    }
+    /**
+     * setOffset
+     *
+     * @param mixed $offset
+     * @access public
+     * @return void
+     */
+    public function setOffset($offset = null)
+    {
+        $this->offset = $offset;
+    }
+    /**
+     * setSort
+     *
+     * @param mixed $sort
+     * @access public
+     * @return void
+     */
+    public function setSort($sort = null)
+    {
+        $this->sort = $sort;
+    }
+
+    /**
      * setOperation
      *
      * @param string $operation
@@ -86,12 +130,18 @@ class Affiliate
      */
     public function getResult()
     {
-        $contents = file_get_contents(sprintf('http://affiliate-api.dmm.com/?api_id=%s&affiliate_id=%s&operation=%s&version=2.00&timestamp=2012-01-13%%2014%%3A08%%3A16&site=%s&service=%s&keyword=%s',
+        $contents = file_get_contents(sprintf('http://%s/?api_id=%s&affiliate_id=%s&operation=%s&version=%s&timestamp=%s&site=%s&service=%s&floor=%s&offset=%d&sort=%s&keyword=%s',
+            self::HOST,
             $this->api_id,
             $this->affiliate_id,
             $this->operation,
+            self::VERSION,
+            urlencode(date('Y-m-d H:i:s')),
             $this->site,
             $this->service,
+            $this->floor,
+            $this->offset,
+            $this->sort,
             urlencode(mb_convert_encoding($this->word, 'EUC-JP', 'UTF-8'))));
 
         $contents = mb_convert_encoding($contents, 'UTF-8', 'EUC-JP');
